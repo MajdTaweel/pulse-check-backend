487ab57adaedc13c4bb4adda1287b2c5067fb344
Majd Taweel <42496516+MajdTaweel@users.noreply.github.com>
2025-07-26 01:24:30 +0300

    chore: set up auth

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = [])
+    if resource.persisted?
+      @token = request.env["warden-jwt_auth.token"]
+      headers["Authorization"] = "Bearer #{@token}"
+
+      render json: {
+        status: {
+          code: 200,
+          message: "Signed up successfully."
+        },
+        data: {
+          user: UserSerializer.new(resource).serializable_hash[:data][:attributes],
+          token: @token
+        }
+      }
+    else
+      render json: {
+        status: { message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
+      }, status: :unprocessable_entity
+    end
+  end
+
+  def respond_to_on_destroy
+    if request.headers["Authorization"].present?
+      jwt_payload = JWT.decode(request.headers["Authorization"].split.last,
+                               Rails.application.credentials.devise_jwt_secret_key!).first
+
+      current_user = User.find(jwt_payload["sub"])
+    end
+
+    if current_user
+      render json: {
+        status: { code: 200, message: "Logged out successfully." }
+      }, status: :ok
+    else
+      render json: {
+        status: { code: 401, message: "Couldn't find an active session." }
+      }, status: :unauthorized
+    end
+  end
+end

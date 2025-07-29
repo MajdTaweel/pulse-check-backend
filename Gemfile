1b3ef78bb65bbc6de7f26a8b18dcc8b45c273697
Majd Taweel <42496516+MajdTaweel@users.noreply.github.com>
2025-07-29 09:50:09 +0300

    deploy: launch & deploy to fly.io

--- Gemfile
+++ Gemfile
@@ -45,6 +45,10 @@ gem "httparty", "~> 0.23.1"
 gem "sidekiq", "~> 8.0"
 gem "sidekiq-scheduler", "~> 6.0"
 
+gem "redis", "~> 5.4"
+
+gem "dockerfile-rails", ">= 1.7", group: :development
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,12 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" } }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" } }
+  end
 end
